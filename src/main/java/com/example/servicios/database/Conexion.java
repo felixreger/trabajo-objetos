010b019144182c3a6615ea1580675d051f84a766
felix <felixregert@gmail.com>
2022-03-08 12:59:17 -0300

    Arreglado el usuario de la base

--- src/main/java/com/example/servicios/database/Conexion.java
+++ src/main/java/com/example/servicios/database/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     String driverClassName = "org.postgresql.Driver";
 	String connectionUrl = "jdbc:postgresql://ec2-18-210-191-5.compute-1.amazonaws.com:5432/d79sq5cldkpff1";
 	String dbUser = "bdycrwycanybkz";
-	String dbPwd = "a68578572591fb6eccd90f4c711aabd61caf1b936c8941db3390ea52e4d3ec70?sslmode=require";
+	String dbPwd = "a68578572591fb6eccd90f4c711aabd61caf1b936c8941db3390ea52e4d3ec70";
 
 	private static Conexion connectionFactory = null;
 
