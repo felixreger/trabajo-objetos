d9edd9988440819529bf97eaa5dfe693caa41229
felix <felixregert@gmail.com>
2022-03-13 14:58:23 -0300

    Implementacion de Filter Servlet

--- src/main/java/com/example/endpoints/auth/cors/CorsFilter.java
+++ src/main/java/com/example/endpoints/auth/cors/CorsFilter.java
@@ -1,32 +1,38 @@
 package com.example.endpoints.auth.cors;
 
-import com.example.endpoints.utils.Utils;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(asyncSupported = true, urlPatterns =  { "/*" })
-public class CorsFilter extends HttpFilter {
+public class CorsFilter implements Filter {
 
     @Override
-    protected void doFilter(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException {
 
-        System.out.println("CORSFilter HTTP Request: " + servletRequest.getMethod());
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
 
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers,Content-Type, Accept, Authorization");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, DELETE, HEAD, PUT, POST");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
 
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
 
         // pass the request along the filter chain
-        chain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, servletResponse);
     }
 
     @Override
